117b34cf54631a9716cb47c928cb5389389cb079
rudokemper <rtakemper@gmail.com>
2024-02-21 17:58:42 -0500

    Basic dashboard display reading from an offline maps table

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="container">
-    <h1>Available Offline Maps</h1>
-    <MapDashboard />
-    <div v-for="item in data" :key="item.id" class="table-item">
-      <h2>{{ item }}</h2>
-    </div>
+  <div>
+    <MapDashboard
+      v-if="dataFetched"
+      :data="data" />
   </div>
 </template>
 
@@ -17,11 +15,7 @@ export default {
       title: "MapPacker: Available Offline Maps",
     };
   },
-  data() {
-    return {
-      data: [],
-    };
-  },
+  components: { MapDashboard },
   async asyncData({ $axios, app }) {
     // Set up the headers for the request
     let headers = {
@@ -32,60 +26,17 @@ export default {
     try {
       // Use the table name in the API request
       const response = await $axios.$get(`/api/data`, { headers });
-      return { data: response };
+      return { 
+        dataFetched: true,
+        data: response 
+      };
     } catch (error) {
       // Handle errors as appropriate
       console.error("Error fetching data:", error);
-      return { data: [] };
+      return {
+        dataFetched: false,
+      };    
     }
   },
 };
 </script>
-
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 2em;
-}
-
-.container h1 {
-  color: #333;
-  margin-bottom: 1em;
-  font-size: 2em;
-  font-weight: 900;
-}
-
-.table-item {
-  background-color: #f9f9f9;
-  border-radius: 5px;
-  padding: 1em;
-  margin-bottom: 1em;
-  width: 50%;
-}
-
-.table-item h2 {
-  color: #333;
-  margin-bottom: 0.5em;
-}
-
-.table-item ul {
-  list-style: none;
-  padding: 0;
-}
-
-.table-item ul li {
-  margin-bottom: 0.5em;
-}
-
-.table-item ul li a {
-  color: #007bff;
-  text-decoration: none;
-}
-
-.table-item ul li a:hover {
-  color: #0056b3;
-  text-decoration: underline;
-}
-</style>
