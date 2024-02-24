1ac8f6594c234780aff4a26ed3b6f77d0b56ccfa
rudokemper <rtakemper@gmail.com>
2024-02-24 11:00:13 -0500

    Store Mapbox instance as non-reactive Vue property

--- components/Map.vue
+++ components/Map.vue
@@ -16,7 +16,6 @@
     ],
     data() {
       return {
-        map: null,
         localLatitude: this.mapboxLatitude,
         localLongitude: this.mapboxLongitude,
         localZoom: this.mapboxZoom,
@@ -24,64 +23,63 @@
     },
     watch: {
       mapboxLatitude(newVal) {
-        if (this.map) {
-          this.map.setCenter([this.localLongitude, newVal]);
+        if (this.$map) {
+          this.$map.setCenter([this.localLongitude, newVal]);
         }
       },
       mapboxLongitude(newVal) {
-        if (this.map) {
-          this.map.setCenter([newVal, this.localLatitude]);
+        if (this.$map) {
+          this.$map.setCenter([newVal, this.localLatitude]);
         }
       },
       mapboxZoom(newVal) {
-        if (this.map) {
-          this.map.setZoom(newVal);
+        if (this.$map) {
+          this.$map.setZoom(newVal);
         }
       },
     },
     mounted() {
       mapboxgl.accessToken = this.mapboxAccessToken;
   
-      const map = new mapboxgl.Map({
+      this.$map = new mapboxgl.Map({
         container: "map",
         style: this.mapboxStyle || "mapbox://styles/mapbox/satellite-streets-v12",
         projection: "mercator",
         center: [this.mapboxLongitude || 0, this.mapboxLatitude || -15],
         zoom: this.mapboxZoom || 2.5,
       });
-      this.map = map;
   
-      this.map.on("load", () => {
+      this.$map.on("load", () => {
         // Navigation Control (zoom buttons and compass)
         const nav = new mapboxgl.NavigationControl();
-        map.addControl(nav, "top-right");
+        this.$map.addControl(nav, "top-right");
   
         // Scale Control
         const scale = new mapboxgl.ScaleControl({
           maxWidth: 80,
           unit: "metric",
         });
-        map.addControl(scale, "bottom-left");
+        this.$map.addControl(scale, "bottom-left");
   
         // Track map center and zoom level
-      map.on("moveend", () => {
-        const center = map.getCenter();
-        this.localLatitude = center.lat;
-        this.localLongitude = center.lng;
-        this.$emit('update:params', {param: 'Latitude', value: center.lat});
-        this.$emit('update:params', {param: 'Longitude', value: center.lng});
-      });
+        this.$map.on("moveend", () => {
+          const center = this.$map.getCenter();
+          this.localLatitude = center.lat;
+          this.localLongitude = center.lng;
+          this.$emit('update:params', {param: 'Latitude', value: center.lat});
+          this.$emit('update:params', {param: 'Longitude', value: center.lng});
+        });
   
-      map.on("zoomend", () => {
-        const zoom = map.getZoom();
-        this.localZoom = zoom;
-        this.$emit('update:params', {param: 'Zoom', value: zoom});
-      });
+        this.$map.on("zoomend", () => {
+          const zoom = this.$map.getZoom();
+          this.localZoom = zoom;
+          this.$emit('update:params', {param: 'Zoom', value: zoom});
+        });
       });
     },
     beforeDestroy() {
-      if (this.map) {
-        this.map.remove();
+      if (this.$map) {
+        this.$map.remove();
       }
     },
   };
