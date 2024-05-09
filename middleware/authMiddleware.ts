cdca10f9f398a03cadcc945df5b3b324cf63f67b
rudokemper <rtakemper@gmail.com>
2024-05-08 22:38:26 -0400

    Gather auth strategy from config

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -4,9 +4,9 @@ const authMiddleware: Middleware = ({ $auth, redirect, route, $config }) => {
   
   const AUTH_STRATEGY = $config.authStrategy;
 
-  if (route.path !== '/login' && AUTH_STRATEGY === "auth0" && !$auth.loggedIn) {
+  if (route.path !== '/login' && AUTH_STRATEGY === 'auth0' && !$auth.loggedIn) {
     return redirect(`/login?redirect=${route.fullPath}`);
-  } else if (route.path !== '/login' && AUTH_STRATEGY === "password" && !$auth.loggedIn) {
+  } else if (route.path !== '/login' && AUTH_STRATEGY === 'password' && !$auth.loggedIn) {
     return redirect(`/login?redirect=${route.fullPath}`);
   }
 };
