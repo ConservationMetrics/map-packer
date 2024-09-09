af3ee45e319d20d68467ff420efd92831480c17f
Rudo Kemper <rtakemper@gmail.com>
2024-09-08 21:57:27 -0400

    Componentize LanguagePicker

--- components/Auth0Login.vue
+++ components/Auth0Login.vue
@@ -1,21 +1,30 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <p class="italic">{{ $t("authMessage") }}.</p>
-    <button
-      class="px-4 py-2 mt-4 mb-4 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
-      @click="loginWithAuth0"
+  <div class="container relative">
+    <div
+      class="absolute top-0 right-0 flex justify-end space-x-4 mt-4 mr-4 mb-4"
     >
-      {{ $t("loginButton") }}
-    </button>
-    <p v-if="errorMessage" class="text-red-500 text-xs italic">
-      {{ errorMessage }}
-    </p>
+      <LanguagePicker />
+    </div>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <p class="italic">{{ $t("authMessage") }}.</p>
+      <button
+        class="px-4 py-2 mt-4 mb-4 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+        @click="loginWithAuth0"
+      >
+        {{ $t("loginButton") }}
+      </button>
+      <p v-if="errorMessage" class="text-red-500 text-xs italic">
+        {{ errorMessage }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
+
+import LanguagePicker from './shared/LanguagePicker.vue';
 
 // Set up composables
 const errorMessage = ref("");
--- components/MapDashboard.vue
+++ components/MapDashboard.vue
@@ -1,45 +1,7 @@
 <template>
   <div class="mt-4 mx-auto w-full max-w-6xl px-4">
     <div class="flex justify-end space-x-4 mb-4">
-      <div class="relative inline-block text-left">
-        <div>
-          <button
-            @click="toggleDropdown"
-            class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
-          >
-            {{ currentLocaleName }}
-            <svg
-              class="-mr-1 ml-2 h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          v-if="dropdownOpen"
-          class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50"
-        >
-          <div class="py-1">
-            <a
-              href="#"
-              v-for="locale in availableLocales"
-              :key="locale.code"
-              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              @click.prevent.stop="changeLocale(locale.code)"
-            >
-              {{ locale.name }}
-            </a>
-          </div>
-        </div>
-      </div>
+      <LanguagePicker></LanguagePicker>
       <NuxtLinkLocale
         to="/map"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer transition-colors duration-200 hidden md:block"
@@ -216,6 +178,8 @@ import { useI18n } from "vue-i18n";
 
 import QRCode from "qrcode.vue";
 import MiniMap from "@/components/MapDashboard/MiniMap.vue";
+import LanguagePicker from './shared/LanguagePicker.vue';
+
 import { copyLink } from "~/utils";
 
 // Define props
