c9fc4d5cb69f3a7b614b0ce032e7608351d6621e
Rudo Kemper <rtakemper@gmail.com>
2024-09-17 09:34:49 -0400

    Utilize useAuth composable in login page

--- pages/login.vue
+++ pages/login.vue
@@ -7,49 +7,11 @@
 
 <script setup>
 import { useHead } from "#imports";
-import { onMounted } from "vue";
 import { useI18n } from "vue-i18n";
 
 // Set up composables
 const { t } = useI18n();
-const router = useRouter();
-const { loggedIn } = useUserSession();
-const localePath = useLocalePath();
-
-// Set up reactive state
-const errorMessage = ref("");
-const redirectPath = ref(localePath("/"));
-
-// On mount
-onMounted(async () => {
-  const redirect = router.currentRoute.value.query.redirect;
-  redirectPath.value = redirect
-    ? decodeURIComponent(redirect)
-    : localePath("/");
-
-  // Imperfect solution to handle Auth0 redirect
-  // Get the code from the URL and redirect to the auth endpoint
-  // Which will then create a session and redirect back to /
-  const urlParams = new URLSearchParams(window.location.search);
-  const code = urlParams.get("code");
-
-  if (code) {
-    window.location.href = `/auth/auth0?code=${code}`;
-  }
-
-  // Handle access denied error
-  const queryParams = new URLSearchParams(window.location.search);
-  const error = queryParams.get("error");
-  const errorDescription = queryParams.get("error_description");
-
-  if (error === "access_denied") {
-    errorMessage.value = decodeURIComponent(errorDescription);
-  }
-
-  if (loggedIn.value) {
-    router.push(redirectPath.value);
-  }
-});
+const { loggedIn, errorMessage } = useAuth();
 
 // Set up page metadata
 useHead({
