91f913e9fe22e6e7d7378133a7c7f5d56aaff0ce
Rudo Kemper <rtakemper@gmail.com>
2024-09-23 16:17:43 -0400

    Get login page working with useAuth composable from shared module

--- pages/login.vue
+++ pages/login.vue
@@ -7,11 +7,17 @@
 
 <script setup>
 import { useHead } from "#imports";
+import { onMounted } from "vue";
 import { useI18n } from "vue-i18n";
 
 // Set up composables
 const { t } = useI18n();
-const { loggedIn, errorMessage } = useAuth();
+const { loggedIn } = useUserSession();
+const errorMessage = ref("");
+
+onMounted(() => {
+  errorMessage.value = useAuth(loggedIn);
+});
 
 // Set up page metadata
 useHead({
