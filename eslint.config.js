a8cbdbd6932a3c708486a0e6ec387804fc1ea1b2
Rudo Kemper <rtakemper@gmail.com>
2024-10-12 00:41:57 -0400

    Use same eslint config as other repos

--- eslint.config.js
+++ eslint.config.js
@@ -15,9 +15,15 @@ export default [
     files: ["**/*.{js,ts,vue}"],
     languageOptions: { parserOptions: { parser: tseslint.parser } },
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": [
+        "warn",
+        { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
+      ],
       "no-undef": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
+      ],
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
@@ -26,5 +32,8 @@ export default [
     rules: {
       "vue/multi-word-component-names": "off",
     },
+  },
+  {
+    ignores: ["public/"],
   },
 ];
