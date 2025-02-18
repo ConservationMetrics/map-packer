1eeb0b58b009026445858b4d728f73fb58cd4732
Rudo Kemper <rtakemper@gmail.com>
2025-02-18 16:30:36 -0500

    Use @nuxt/eslint convention for config

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,17 @@
-import globals from "globals";
-import js from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
-import gitignore from "eslint-config-flat-gitignore";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  gitignore(),
-  { files: ["**/*.{js,ts,vue}"] },
-  { languageOptions: { globals: globals.browser } },
-  js.configs.recommended,
-  {
-    files: ["**/*.{js,ts,vue}"],
-    languageOptions: { parserOptions: { parser: tsParser } },
-    rules: {
-      "no-unused-vars": [
-        "warn",
-        { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
-      ],
-      "no-undef": "warn",
-      "vue/html-self-closing": [
-        "error",
-        {
-          html: {
-            void: "always",
-            normal: "never",
-            component: "always",
-          },
+export default withNuxt().override("nuxt/vue/rules", {
+  rules: {
+    "vue/require-default-prop": "off",
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "always",
         },
-      ],
-    },
+      },
+    ],
   },
-  {
-    ignores: ["public/"],
-  },
-);
+});
